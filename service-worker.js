eeb2e37d3f82f5e991d19cdb5be7d3fdbf4b0e58
pimsa1989 <veerakulvinij.p@gmail.com>
2025-05-16 15:34:14 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,1 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("smith-menu-cache").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
-    })
-  );
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
-  );
-});
+// Service Worker
